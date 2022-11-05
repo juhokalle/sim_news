906a56dcf47679f5ff110e3c087f4f8196f188eb
juhokalle <juhokallekois@gmail.com>
2022-11-05 19:02:55 +0200

    test creds

--- mp1/b_slurm.sh
+++ mp1/b_slurm.sh
@@ -13,7 +13,7 @@ N_MODS_PER_CORE=5
 MANUALLY_ASSIGNED_ID=20221102
 SRUN_CPUS_PER_TASK=16
 
-module use /appl/modulefiles/all/
+module use /appl/modulefiles/all/ #loads all mds
 module load R/4.2.1
 
 srun Rscript b_Rscript4slurm.R $N_MODS_PER_CORE $SLURM_ARRAY_TASK_ID $SLURM_JOB_ID $MANUALLY_ASSIGNED_ID $SLURM_JOB_NAME $SRUN_CPUS_PER_TASK
