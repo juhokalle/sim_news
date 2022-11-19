7e2d37046a23d1c9092771d736ff952a557c953c
juhokalle <juhokallekois@gmail.com>
2022-11-19 22:00:46 +0200

    data

--- mp1/b_Rscript4slurm.R
+++ mp1/b_Rscript4slurm.R
@@ -102,7 +102,7 @@ tt_optim_parallel = tt %>%
   # generate initial values and likelihood functions (we can use the same template for initial values and likelihood fct bc both have no parameters for density)
   mutate(theta_init = map2(tmpl, data_list, ~get_init_armamod_whf_random(.y, .x))) %>% 
   # standardize data for estimation
-  mutate(data_list = data_list %>% map(~apply(.x, 2, function(x) x/sd(x)))) %>% 
+  # mutate(data_list = data_list %>% map(~apply(.x, 2, function(x) x/sd(x)))) %>% 
   select(theta_init, tmpl, data_list)
 
 params_parallel = lapply(1:nrow(tt_optim_parallel),
