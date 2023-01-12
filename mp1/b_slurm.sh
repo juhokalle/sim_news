e6b79b0041fc2655ee9f58f035b69b7e19ccab0b
juhokalle <juhokallekois@gmail.com>
2023-01-12 16:21:23 +0200

    qq

--- mp1/b_slurm.sh
+++ mp1/b_slurm.sh
@@ -2,14 +2,14 @@
 #SBATCH --job-name=mp_svarma0
 #SBATCH -N 1
 #SBATCH -p short
-#SBATCH -t 01:00:00
+#SBATCH -t 01:15:00
 #SBATCH -c 16
 #SBATCH --mem=1000
 #SBATCH -o rro%a.out
 #SBATCH -e rre%a.err
-#SBATCH --array=1-45
+#SBATCH --array=1-54
 
-N_MODS_PER_CORE=4
+N_MODS_PER_CORE=5
 MANUALLY_ASSIGNED_ID=20230110
 SRUN_CPUS_PER_TASK=16
 
