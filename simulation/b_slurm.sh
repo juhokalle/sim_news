d55483f7e5c7943400ba947ede2ba8c89fb1efba
juhokalle <juhokallekois@gmail.com>
2023-04-08 01:17:37 +0300

    slurm

--- simulation/b_slurm.sh
+++ simulation/b_slurm.sh
@@ -4,11 +4,11 @@
 #SBATCH -N 1
 #SBATCH -p short
 #SBATCH -t 01:00:00
-#SBATCH -n 20
+#SBATCH -n 40
 #SBATCH --mem=1000
 #SBATCH -o rro%a.out
 #SBATCH -e rre%a.err
-#SBATCH --array=1-100
+#SBATCH --array=1-50
 #SBATCH --constraint=amd
 
 MANUALLY_ASSIGNED_ID=20230407
