be5d4fbfbe88cd776914f63e7fd946a7f545ec23
juhokalle <juhokallekois@gmail.com>
2023-06-14 15:30:08 +0300

    asy simu

--- simulation/d_simu_mixed_mod.R
+++ simulation/d_simu_mixed_mod.R
@@ -19,10 +19,10 @@ void = lapply(pkgs, function(x) suppressMessages(library(x, character.only = TRU
 
 # SIMU MODEL ####
 DIM_OUT <- 3
+n_obs <- 2.5*1e4
 mod_mat <- get_struc_mat(model_type = "dynamic")
 mod_mat$eps_val <- 10^-12
 mod_str <- do.call(solve_re_mod_bp, mod_mat)
-sign_mat <- sign(mod_str$sigma_L)
 ma_polm <- abind::abind(diag(DIM_OUT),
                         with(mod_str, sigma_L%*%diag(c(0, 0.5, 2))%*%solve(sigma_L)), 
                         along = 3) %>% polm
@@ -38,45 +38,44 @@ params$IX_ARRAY_JOB = as.integer(args[1]) # index of array-job. Number of array-
 params$SLURM_JOB_ID = as.integer(args[2])
 params$MANUALLY_ASSIGNED_ID = as.integer(args[3])
 params$SLURM_ARRAY_TASK_MAX = as.integer(args[4])
+params$NEW_DIR = args[5]
 
 # OPTIMIZATION PARAMS
-params$RESTART_W_NOISE = 0
+
+## general
+params$RESTART_W_NOISE = 1
+params$PERM_INIT = 5
 params$FIX_INIT = FALSE
+params$IC <- TRUE
 params$penalty_prm = 100
-params$IC <- FALSE
+params$DIM_OUT = DIM_OUT
 
-params$IT_OPTIM_GAUSS = 3
-params$USE_BFGS_GAUSS = TRUE
-params$USE_NM_GAUSS = TRUE
-params$MAXIT_BFGS_GAUSS = 500
+## gaussian density
+params$IT_OPTIM_GAUSS = 1
+params$USE_BFGS_GAUSS = FALSE
+params$USE_NM_GAUSS = FALSE
+params$USE_CS_GAUSS = FALSE
+params$MAXIT_BFGS_GAUSS = 200
 params$MAXIT_NM_GAUSS = 3000
+params$MAXIT_CS_GAUSS = 500
 
-params$IT_OPTIM_LAPLACE = 3
+## laplacian density
+params$IT_OPTIM_LAPLACE = 2
 params$USE_BFGS_LAPLACE = TRUE
 params$USE_NM_LAPLACE = TRUE
-params$MAXIT_BFGS_LAPLACE = 500
-params$MAXIT_NM_LAPLACE = 3000
+params$USE_CS_LAPLACE = FALSE
+params$MAXIT_BFGS_LAPLACE = 100 # default for derivative based methods
+params$MAXIT_NM_LAPLACE = 1000 # default for NM is 500
+params$MAXIT_CS_LAPLACE = 500
 
-params$IT_OPTIM_SGT = 4
+## sgt density
+params$IT_OPTIM_SGT = 2
 params$USE_BFGS_SGT = TRUE
 params$USE_NM_SGT = TRUE
-params$MAXIT_BFGS_SGT = 1000
-params$MAXIT_NM_SGT = 3000
-
-params$PATH_RESULTS_HELPER = "/proj/juhokois/sim_news/local_data/"
-params$DIM_OUT <- DIM_OUT
-
-# SIMULATION SPECS: TARGET PATH FOR SAVING RESULTS
-pap = pap_factory(params$PATH_RESULTS_HELPER)
-
-# New directory for saving all tibbles
-new_dir_path = pap(paste0("jobid_", params$MANUALLY_ASSIGNED_ID))
-
-if(params$IX_ARRAY_JOB==1){
-  if (!dir.exists(new_dir_path)){
-    dir.create(new_dir_path)
-  }
-}
+params$USE_CS_SGT = FALSE
+params$MAXIT_BFGS_SGT = 100 # default for derivative based methods
+params$MAXIT_NM_SGT = 1000 # default for NM is 500
+params$MAXIT_CS_SGT = 500
 
 # GENERATE DATA
 rg_fun <- 
@@ -90,9 +89,9 @@ rg_fun <-
 
 DATASET = do.call(what = simu_y, 
                   args = list(model = dgp_mod, 
-                              n.obs = 100000,
+                              n.obs = n_obs,
                               rand.gen = rg_fun$fun,
-                              n.burnin = 500))$y
+                              n.burnin = n_obs*2))$y
 
 # MODEL SPECIFICATION
 tt =
@@ -104,22 +103,24 @@ tt =
   mutate(n_st = DIM_OUT * q - n_unst) %>% 
   mutate(kappa = n_unst %/% DIM_OUT,
          k = n_unst %% DIM_OUT) %>% 
-  # Assume correct specification w.r.t. MA polm
+  # assume correct specification w.r.t. MA polm
   filter(n_unst==1) %>% 
-  # This way of including data makes it convenient for slicing
-  expand_grid(data_list = list(DATASET)) %>% 
-  # Save sd's and s'dize data
+  mutate(data_list = list(DATASET)) %>% 
   mutate(sd_vec = map(.x = data_list, ~ apply(.x, 2, sd))) %>% 
-  mutate(data_list = map(.x = data_list, ~ apply(.x, 2, function(x) (x-mean(x))/sd(x))))
-
-# Parallel setup ####
-tt_optim_parallel = tt %>% 
+  mutate(data_list = map(.x = data_list, ~ apply(.x, 2, function(x) (x-mean(x))/sd(x)))) %>% 
   # template
   mutate(tmpl = pmap(., pmap_tmpl_whf_rev)) %>% 
   # generate initial values and likelihood functions (we can use the same template for initial values and likelihood fct bc both have no parameters for density)
-  mutate(theta_init = map2(tmpl, data_list, ~get_init_armamod_whf_random(.y, .x))) %>% 
-  mutate(shock_distr = "tdist") %>% 
-  select(theta_init, tmpl, data_list, shock_distr)
+  mutate(theta_init = map2(.x = data_list, .y = tmpl, ~get_init_armamod_whf_random(.x, .y))) %>%
+  mutate(theta_init = map2(.x = theta_init, .y = tmpl, ~ perm_init(.x, params$PERM_INIT, .y))) %>% 
+  unnest_longer(theta_init) %>% 
+  mutate(init_ix = rep(1:(params$PERM_INIT+1), n()/(params$PERM_INIT+1))) %>% 
+  mutate(shock_distr = "tdist")
+
+# Parallel setup ####
+tt_optim_parallel = tt %>%
+  slice(params$IX_ARRAY_JOB) %>% 
+  dplyr::select(theta_init, tmpl, data_list, shock_distr)
 
 params_parallel = lapply(1:nrow(tt_optim_parallel),
                          function(i) t(tt_optim_parallel)[,i])
@@ -131,18 +132,16 @@ tibble_out =
   unnest_wider(value) %>% 
   unnest_wider(results_list) %>%
   unnest_wider(input_integerparams) %>% 
-  mutate(tt) %>% 
-  mutate(shock_distr = "tdist") %>% 
-  mutate(tmpl = pmap(., pmap_tmpl_whf_rev)) %>%
+  mutate(tt %>% slice(params$IX_ARRAY_JOB)) %>% 
   mutate(irf = map2(.x = params_deep_final, .y = tmpl, ~ irf_whf(.x, .y, n_lags = 12))) %>% 
   mutate(irf = map2(.x = sd_vec, .y = irf, ~ diag(.x)%r%.y)) %>% 
-  mutate(rmat = map(.x = irf, ~ choose_perm_sign(target_mat = sign_mat, 
-                                                 cand_mat = unclass(.x)[,,1], 
-                                                 type = "frob"))) %>%
+  mutate(true_irf = list(with(dgp_mod, pseries(sys, 12)%r%sigma_L))) %>% 
+  mutate(rmat = map2(.x = true_irf, .y = irf, ~ choose_perm_sign(target_mat = .x, 
+                                                                 cand_mat = .y, 
+                                                                 type = "min_rmse"))) %>%
   mutate(irf = map2(.x = irf, .y = rmat, ~ .x%r%.y)) %>% 
-  expand_grid(nobs = nrow(DATASET), rg = rg_fun$lbl) %>% 
-  select(p, q, kappa, k, n_st, n_unst, value_final, irf, nobs, rg)
-  
+  expand_grid(rg = rg_fun$lbl)
+
 tibble_id <- paste0("/tibble_",
                     paste(sample(0:9, 5, replace = TRUE), collapse = ""), 
                     paste(sample(letters, 5), collapse = ""),
