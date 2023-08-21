a3440d61eef1f823bbfdd98fc4758cb2dd4d3176
juhokalle <juhokallekois@gmail.com>
2023-08-21 21:25:20 +0300

    fix compilation

--- mp1/compile_results.R
+++ mp1/compile_results.R
@@ -5,7 +5,7 @@ tibble_path <- local_data %>%
   paste0(list.files(local_data)[grepl("jobid_", list.files(local_data))], "/")
 tibble_out <- paste0(tibble_path, list.files(tibble_path)) %>%
   map_dfr(readRDS) %>% 
-  group_by(p, q, kappa, k, shock_distr) %>%
+  group_by(p, q, kappa, k, shock_distr, data_list) %>%
   slice_min(value_final) %>%
   ungroup()
 saveRDS(tibble_out, paste0(local_data, "tt_simu.rds"))
